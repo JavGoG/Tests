4c4ee36a2ffbeaa945f4a3ebf06291a7ae67459d
JavGog <javierggjavier@gmail.com>
2023-11-25 18:28:30 +0100

    Initial commit

---
+++ src/main/java/com/javier/avila/AvilaApplication.java
@@ -0,0 +1,13 @@
+package com.javier.avila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvilaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvilaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javier/avila/AvilaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javier.avila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvilaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
